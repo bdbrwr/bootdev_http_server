303d4c33b304329b44f4addae5c78f40279e7cec
bdbrwr <bart@bdbrwr.nl>
2025-05-31 03:56:52 +0200

    Middleware inc metric homepage hits

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // Increment the counter
+		next.ServeHTTP(w, r)      // Call the next handler in the chain
+	})
+}
+
+func (cfg *apiConfig) handlerMetricsInc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	currentHits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", currentHits)))
+}
+
+func (cfg *apiConfig) handlerMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("File server hits reset"))
+}
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
+	apiConfig := apiConfig{}
+	apiConfig.fileserverHits.Store(0)
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiConfig.handlerMetricsInc)
+	mux.HandleFunc("/reset", apiConfig.handlerMetricsReset)
 
 	server := http.Server{
 		Handler: mux,
