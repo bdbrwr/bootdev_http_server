9dc3a94c3cfff0e6d1bcc74b2daecd983d0cc08c
bdbrwr <bart@bdbrwr.nl>
2025-05-31 03:29:20 +0200

    healthz endpoint, moved static files to /app

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const filepathRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := http.Server{
 		Handler: mux,
@@ -19,4 +20,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
