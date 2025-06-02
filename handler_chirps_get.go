c1b3a8945d590b90b264494246d72cbf25aa8fe8
bdbrwr <bart@bdbrwr.nl>
2025-06-02 15:00:43 +0200

    Get signular chirp

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
@@ -22,4 +28,28 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 
 	respondWithJSON(w, http.StatusOK, chirps)
 
+}
+
+func (cfg *apiConfig) handlerChirpRetrieve(w http.ResponseWriter, r *http.Request) {
+	id, _ := uuid.Parse(r.PathValue("chirpID"))
+	fmt.Println("uuid parsed")
+	dbChirp, err := cfg.db.GetChirp(r.Context(), id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusNotFound, "No chirp found", err)
+		} else {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirp", err)
+		}
+		return
+	}
+
+	chirp := Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusOK, chirp)
 }
