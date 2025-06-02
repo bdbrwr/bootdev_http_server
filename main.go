99221840a663000e2fa730e674126af1b06194b7
bdbrwr <bart@bdbrwr.nl>
2025-06-02 13:55:29 +0200

    Get all chirps

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGetAll)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
